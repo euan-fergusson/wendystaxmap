aa22bee082ebcd1a35d2761938fad89b1b72fa9e
Euan Fergusson <euan@ionadigital.com>
2024-10-14 10:41:20 +0100

    connection

--- config/database.js
+++ config/database.js
@@ -1,26 +1,16 @@
 const path = require("path");
 const config = require("platformsh-config").config();
-
 let dbRelationship = "postgresdatabase";
 
-// Strapi default sqlite settings.
-let connection = {
-  connection: {
-    client: "sqlite",
-    connection: {
-      filename: path.join(
-        __dirname,
-        "..",
-        process.env.DATABASE_FILENAME || ".tmp/data.db"
-      ),
-    },
-    useNullAsDefault: true,
-  },
-};
+if (config.isValidPlatform() && config.inBuild()) {
 
-if (config.isValidPlatform() && !config.inBuild()) {
+
+  console.log("================================");
+  console.log("hello world");
+  console.log("================================");
   // Platform.sh database configuration.
   const credentials = config.credentials(dbRelationship);
+  console.log("hello world again");
   console.log(
     `Using Platform.sh configuration with relationship ${dbRelationship}.`
   );
@@ -50,21 +40,26 @@ if (config.isValidPlatform() && !config.inBuild()) {
       pool,
     },
   };
-} else {
-  if (config.isValidPlatform()) {
-    // Build hook configuration message.
-    console.log(
-      "Using default configuration during Platform.sh build hook until relationships are available."
-    );
-  } else {
-    // Strapi default local configuration.
-    console.log(
-      "Not in a Platform.sh Environment. Using default local sqlite configuration."
-    );
-  }
+  
+  module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: credentials.ip,
+          port: credentials.port,
+          database: credentials.path,
+          username: credentials.username,
+          password: credentials.password,
+        },
+        options: {
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+      },
+    },
+  });
 }
 
-console.log(connection);
 
-// export strapi database connection
-module.exports = ({ env }) => connection;
